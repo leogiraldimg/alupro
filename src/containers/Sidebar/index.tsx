da5246aeea415e632289eacb8977a97267b3d2f3
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2024-09-30 14:18:28 -0300

    feat: ajusta barra lateral
    Este commit implementa o comportamento responsivo da barra lateral.

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -9,56 +9,73 @@ import { Link } from "react-router-dom";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import MoneyOutlined from "@mui/icons-material/MoneyOutlined";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 
 import { COLOR_DARK_SLATE_GRAY, COLOR_VANILLA } from "@/constants/colors";
 
 const Sidebar: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const [toggled, setToggled] = useState(false);
+    const [broken, setBroken] = useState(false);
 
     return (
-        <SidebarComp
-            rootStyles={{
-                [`.${sidebarClasses.container}`]: {
-                    backgroundColor: COLOR_DARK_SLATE_GRAY,
-                },
-            }}
-            className="h-[100vh]"
-            collapsed={collapsed}
-        >
-            <Menu
-                menuItemStyles={{
-                    button: {
-                        color: COLOR_VANILLA,
-                        ":hover": {
-                            color: COLOR_DARK_SLATE_GRAY,
-                            backgroundColor: COLOR_VANILLA,
-                        },
+        <>
+            {broken && (
+                <button
+                    className="fixed top-2 left-2 z-10"
+                    onClick={() => setToggled(!toggled)}
+                >
+                    <FormatAlignJustifyIcon />
+                </button>
+            )}
+            <SidebarComp
+                rootStyles={{
+                    [`.${sidebarClasses.container}`]: {
+                        backgroundColor: COLOR_DARK_SLATE_GRAY,
                     },
                 }}
+                className="h-[100vh]"
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onBreakPoint={setBroken}
+                onBackdropClick={() => setToggled(false)}
             >
-                <MenuItem
-                    icon={<MenuOutlinedIcon />}
-                    onClick={() => {
-                        setCollapsed(!collapsed);
+                <Menu
+                    menuItemStyles={{
+                        button: {
+                            color: COLOR_VANILLA,
+                            ":hover": {
+                                color: COLOR_DARK_SLATE_GRAY,
+                                backgroundColor: COLOR_VANILLA,
+                            },
+                        },
                     }}
-                    className="text-center"
                 >
-                    <h1 className="font-bold">AluPro</h1>
-                </MenuItem>
-                <MenuItem
-                    component={<Link to={"/"} />}
-                    icon={<HomeOutlinedIcon />}
-                >
-                    Home
-                </MenuItem>
-                <MenuItem
-                    component={<Link to={"/calcula-preco"} />}
-                    icon={<MoneyOutlined />}
-                >
-                    Calcula Preço
-                </MenuItem>
-            </Menu>
-        </SidebarComp>
+                    <MenuItem
+                        icon={<MenuOutlinedIcon />}
+                        onClick={() => {
+                            setCollapsed(!collapsed);
+                        }}
+                        className="text-center"
+                    >
+                        <h1 className="font-bold">AluPro</h1>
+                    </MenuItem>
+                    <MenuItem
+                        component={<Link to={"/"} />}
+                        icon={<HomeOutlinedIcon />}
+                    >
+                        Home
+                    </MenuItem>
+                    <MenuItem
+                        component={<Link to={"/calculate-price"} />}
+                        icon={<MoneyOutlined />}
+                    >
+                        Calcula Preço
+                    </MenuItem>
+                </Menu>
+            </SidebarComp>
+        </>
     );
 };
 
